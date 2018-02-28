e7e50fddd5ae4f1de512714a515efba65a8b5076
Andy - Original Repository <andy+travis@andrewwegner.com>
2018-02-28 00:08:44 -0600

    Update hello.py
     - Show that Travis CI does simple string matching to hide environment variable

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+import os
+
 print("Hello World!")
+aws_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret = os.environ['AWS_SECRET_ACCESS_KEY']
+print("AWS KEY ID: |{}{}|".format(aws_id[:len(aws_id)//2], aws_id[len(aws_id)//2:]))
+print("AWS SECRET KEY: |{} {}|".format(aws_secret[:len(aws_secret)//2], aws_secret[len(aws_secret)//2:]))
